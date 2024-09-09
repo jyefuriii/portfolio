7693944cee1fe858d0b4b9e200b0af19b968c222
jyefuriii <jeffrey.t.fabella@gmail.com>
2024-09-10 06:00:49 +0800

    Save local changes before merging

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import Footer from "./Components/Footer";
 function App() {
   return (
     /*
-    <Router>
-    */
     <Router basename={ProcessingInstruction.env.PUBLIC_URL}>
+    */
+    <Router>
       <Header />
       <div className="App">
         <Routes>
