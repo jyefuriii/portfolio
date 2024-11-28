a9a8dbd9e48d926953e5bc8ce3a97be831022dd0
jyefuriii <143618156+jyefuriii@users.noreply.github.com>
2024-11-29 02:40:02 +0800

    Update App.js included basename

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     /* <Router>*/
 
-    <Router /*basename={process.env.PUBLIC_URL}*/>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <div className="App">
         <Routes>
